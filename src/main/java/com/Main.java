364bbb5e09b81eaedb7aa4a0a129960dad39794a
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2022-10-28 08:47:24 -0300

    Clean main file

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,10 +1,6 @@
 package main.java.com;
 
-import main.java.com.enums.JobTitleEnum;
 import main.java.com.service.NormalizingService;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
@@ -14,19 +10,7 @@ public class Main {
 
         System.out.println(service.normalizeTitle("Java engineer"));
         System.out.println(service.normalizeTitle("Java enginer"));
-        System.out.println(service.normalizeTitle("teacher"
-        ));
+        System.out.println(service.normalizeTitle("teacher"));
 
-        Map<String, String> map = new HashMap<>();
-        map.put("Engineer", JobTitleEnum.ENGINEER.getDescription());
-        map.put("Accountant", JobTitleEnum.ACCOUNTANT.getDescription());
-        System.out.println(map.get("oi"));
-        System.out.println(map.get("Engineer"));
-
-//        System.out.println(NormalizingService.normalizeTitle("C# engineer"));
-//
-//        System.out.println(NormalizingService.normalizeTitle("Cloud Architect"));
-//        System.out.println(NormalizingService.normalizeTitle("Senior Architect"));
-//        System.out.println(NormalizingService.normalizeTitle("chief Accountan"));
     }
 }
